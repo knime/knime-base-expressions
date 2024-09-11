e6292a0521cc64f21b710abedd3a2813e1f0ab3f
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2024-09-11 09:52:10 +0200

    AP-23177: Abstract applying filter expr between model and dialog
    AP-23177 (Unnecessary materialization in expression row mapper)

--- org.knime.base.expressions/src/main/java/org/knime/base/expressions/node/row/filter/ExpressionRowFilterNodeModel.java
+++ org.knime.base.expressions/src/main/java/org/knime/base/expressions/node/row/filter/ExpressionRowFilterNodeModel.java
@@ -53,6 +53,8 @@ import static org.knime.base.expressions.ExpressionRunnerUtils.flowVarToTypeForT
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
+import java.util.function.Consumer;
 
 import org.knime.base.expressions.ExpressionRunnerUtils;
 import org.knime.base.expressions.node.NodeExpressionMapperContext;
@@ -61,7 +63,6 @@ import org.knime.core.data.columnar.table.VirtualTableIncompatibleException;
 import org.knime.core.data.columnar.table.virtual.ColumnarVirtualTable;
 import org.knime.core.data.columnar.table.virtual.ColumnarVirtualTableMaterializer;
 import org.knime.core.expressions.Ast;
-import org.knime.core.expressions.EvaluationContext;
 import org.knime.core.expressions.Expressions;
 import org.knime.core.expressions.Expressions.ExpressionCompileException;
 import org.knime.core.node.BufferedDataTable;
@@ -73,6 +74,7 @@ import org.knime.core.node.Node;
 import org.knime.core.node.NodeModel;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.workflow.FlowVariable;
 import org.knime.core.table.virtual.spec.SourceTableProperties.CursorType;
 
 /**
@@ -91,13 +93,14 @@ final class ExpressionRowFilterNodeModel extends NodeModel {
     }
 
     /** @return the typed Ast for the configured expression */
-    private Ast getPreparedExpression(final DataTableSpec inSpec, final String script)
-        throws ExpressionCompileException {
+    private static Ast getPreparedExpression(final String expression, final DataTableSpec inSpec,
+        final Map<String, FlowVariable> availableFlowVariables) throws ExpressionCompileException {
 
-        var ast = Expressions.parse(script);
-        Expressions.inferTypes(ast, columnToTypesForTypeInference(inSpec), flowVarToTypeForTypeInference(
-            getAvailableInputFlowVariables(ExpressionRunnerUtils.SUPPORTED_FLOW_VARIABLE_TYPES)));
-
+        var ast = Expressions.parse(expression);
+        Expressions.inferTypes(ast, //
+            columnToTypesForTypeInference(inSpec), //
+            flowVarToTypeForTypeInference(availableFlowVariables) //
+        );
         return ast;
     }
 
@@ -115,39 +118,15 @@ final class ExpressionRowFilterNodeModel extends NodeModel {
     @Override
     protected BufferedDataTable[] execute(final BufferedDataTable[] inData, final ExecutionContext exec)
         throws Exception {
-
-        var inTable = inData[0];
-        var numberOfRows = inTable.size();
-
         var messageBuilder = createMessageBuilder();
-        EvaluationContext ctx = messageBuilder::addTextIssue;
 
-        exec.setProgress(0, "Evaluating expression");
-
-        // Prepare the expression
-        var expression = getPreparedExpression(inTable.getDataTableSpec(), m_settings.getScript());
-
-        // Create a reference table for the input table
-        var inRefTable = ExpressionRunnerUtils.createReferenceTable(inTable, exec, exec.createSubProgress(0.33));
-
-        // Pre-evaluate the aggregations
-        // NB: We use the inRefTable because it is guaranteed to be a columnar table
-        ExpressionRunnerUtils.evaluateAggregations(expression, inRefTable.getBufferedTable(),
-            exec.createSubProgress(0.33));
-
-        // Evaluate the expression and materialize the result
-        var exprContext = new NodeExpressionMapperContext(this::getAvailableInputFlowVariables);
-
-        // We must avoid using inRefTable.getVirtualTable() directly. Doing so would result in building upon the
-        // transformation of the input table, instead of initiating a new fragment. This approach leads to complications
-        // when loading the virtual table, as it would attempt to resolve the sources of the input table. By creating a
-        // new ColumnarVirtualTable, we establish a new SourceTableTransform that references the input table. This
-        // ensures that the input table itself acts as the source, providing a clean slate for transformations.
-        // Note that the CursorType is irrelevant because the transform gets re-sourced for running the comp graph.
-        var inputVirtualTable = new ColumnarVirtualTable(inRefTable.getId(), inRefTable.getSchema(), CursorType.BASIC);
-
-        var filteredTable = ExpressionRunnerUtils.filterTableByExpression(inputVirtualTable, expression, numberOfRows,
-            ctx, exprContext);
+        var outputTable = applyFilterExpression( //
+            m_settings.getScript(), //
+            inData[0], //
+            getAvailableFlowVariables(ExpressionRunnerUtils.SUPPORTED_FLOW_VARIABLE_TYPES), //
+            exec, //
+            messageBuilder::addTextIssue //
+        );
 
         var issueCount = messageBuilder.getIssueCount();
         if (issueCount > 0) {
@@ -159,25 +138,67 @@ final class ExpressionRowFilterNodeModel extends NodeModel {
             setWarning(message);
         }
 
-        BufferedDataTable outputTable;
-        try {
-            outputTable = ColumnarVirtualTableMaterializer.materializer() //
-                .sources(inRefTable.getSources()) //
-                .materializeRowKey(true) //
-                .progress((rowIndex, rowKey) -> {
-                }) //
-                .executionContext(exec) //
-                .tableIdSupplier(Node.invokeGetDataRepository(exec)::generateNewID) //
-                .materialize(filteredTable) //
-                .getBufferedTable();
-        } catch (VirtualTableIncompatibleException e) {
-            throw new IllegalStateException("This is an implementation error. Must not happen "
-                + "because the table is guaranteed to be compatible.", e);
-        } catch (CanceledExecutionException e) {
-            throw new IllegalStateException("Preview evaluation cancelled by the user, which should be impossible", e);
-        }
-
         return new BufferedDataTable[]{outputTable};
+    }
+
+    /**
+     * Applies the given filter expression to the input table and returns the resulting table.
+     *
+     * @param expression the filter expression
+     * @param inputTable the input table
+     * @param availableFlowVariables the available flow variables
+     * @param exec the execution context
+     * @param setWarning a consumer for setting warnings
+     * @return the filtered table
+     * @throws ExpressionCompileException if the expression could not be compiled
+     * @throws CanceledExecutionException if the execution was cancelled
+     * @throws VirtualTableIncompatibleException
+     */
+    public static BufferedDataTable applyFilterExpression( //
+        final String expression, //
+        final BufferedDataTable inputTable, //
+        final Map<String, FlowVariable> availableFlowVariables, //
+        final ExecutionContext exec, //
+        final Consumer<String> setWarning //
+    ) throws ExpressionCompileException, CanceledExecutionException, VirtualTableIncompatibleException {
+        var numRows = inputTable.size();
+        exec.setProgress(0, "Evaluating expression");
+
+        // Parse the expression and infer the types
+        var ast = getPreparedExpression(expression, inputTable.getDataTableSpec(), availableFlowVariables);
+
+        // Create a reference table for the input table
+        var inRefTable = ExpressionRunnerUtils.createReferenceTable(inputTable, exec, exec.createSubProgress(0.33));
+
+        // Pre-evaluate the aggregations
+        // NB: We use the inRefTable because it is guaranteed to be a columnar table
+        ExpressionRunnerUtils.evaluateAggregations(ast, inRefTable.getBufferedTable(), exec.createSubProgress(0.33));
+
+        // Evaluate the expression and materialize the result
+        var exprContext = new NodeExpressionMapperContext(x -> availableFlowVariables);
+
+        // We must avoid using inRefTable.getVirtualTable() directly. Doing so would result in building upon the
+        // transformation of the input table, instead of initiating a new fragment. This approach leads to complications
+        // when loading the virtual table, as it would attempt to resolve the sources of the input table. By creating a
+        // new ColumnarVirtualTable, we establish a new SourceTableTransform that references the input table. This
+        // ensures that the input table itself acts as the source, providing a clean slate for transformations.
+        // Note that the CursorType is irrelevant because the transform gets re-sourced for running the comp graph.
+        var inputVirtualTable = new ColumnarVirtualTable(inRefTable.getId(), inRefTable.getSchema(), CursorType.BASIC);
+
+        var filteredTable = ExpressionRunnerUtils.filterTableByExpression(inputVirtualTable, ast, inputTable.size(),
+            setWarning::accept, exprContext);
+
+        var materializeProgress = exec.createSubProgress(0.34);
+        return ColumnarVirtualTableMaterializer.materializer() //
+            .sources(inRefTable.getSources()) //
+            .materializeRowKey(true) //
+            .progress((rowIndex, rowKey) -> materializeProgress.setProgress(rowIndex / (double)numRows, //
+                () -> "Evaluating expression (row %d of %s)".formatted(rowIndex + 1, numRows)) //
+            ) //
+            .executionContext(exec) //
+            .tableIdSupplier(Node.invokeGetDataRepository(exec)::generateNewID) //
+            .materialize(filteredTable) //
+            .getBufferedTable();
     }
 
     @Override
--- org.knime.base.expressions/src/main/java/org/knime/base/expressions/node/row/filter/ExpressionRowFilterNodeScriptingService.java
+++ org.knime.base.expressions/src/main/java/org/knime/base/expressions/node/row/filter/ExpressionRowFilterNodeScriptingService.java
@@ -58,22 +58,16 @@ import org.knime.base.expressions.ExpressionRunnerUtils;
 import org.knime.base.expressions.node.ExpressionCodeAssistant;
 import org.knime.base.expressions.node.ExpressionDiagnostic;
 import org.knime.base.expressions.node.ExpressionDiagnostic.DiagnosticSeverity;
-import org.knime.base.expressions.node.NodeExpressionMapperContext;
+import org.knime.base.expressions.node.row.InputTableCache;
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.columnar.table.VirtualTableIncompatibleException;
-import org.knime.core.data.columnar.table.virtual.ColumnarVirtualTableMaterializer;
-import org.knime.core.data.columnar.table.virtual.reference.ReferenceTable;
 import org.knime.core.expressions.Ast;
-import org.knime.core.expressions.EvaluationContext;
 import org.knime.core.expressions.Expressions;
 import org.knime.core.expressions.Expressions.ExpressionCompileException;
 import org.knime.core.expressions.ReturnResult;
 import org.knime.core.expressions.ValueType;
 import org.knime.core.node.BufferedDataTable;
 import org.knime.core.node.CanceledExecutionException;
-import org.knime.core.node.ExecutionMonitor;
-import org.knime.core.node.Node;
-import org.knime.core.node.NodeLogger;
 import org.knime.core.node.workflow.NativeNodeContainer;
 import org.knime.core.node.workflow.NodeContext;
 import org.knime.scripting.editor.ScriptingService;
@@ -83,7 +77,6 @@ import org.knime.scripting.editor.ScriptingService;
  *
  * @author Tobias Kampmann, TNG, Germany
  */
-@SuppressWarnings("restriction")
 final class ExpressionRowFilterNodeScriptingService extends ScriptingService {
 
     private static final int PREVIEW_MAX_ROWS = 1000;
@@ -94,39 +87,18 @@ final class ExpressionRowFilterNodeScriptingService extends ScriptingService {
      */
     private Function<String, ReturnResult<ValueType>> m_columnToType;
 
-    /**
-     * Cached input table for executing the expression.
-     */
-    private ReferenceTable m_inputTable;
-
-    /**
-     * Cached row count of the input table. In case of a row-based table a columnar-based table is created on the fly
-     * for the first {@link PREVIEW_MAX_ROWS} rows, thus loosing the information about the original input table row
-     * count
-     */
-    private long m_inputTableRowCount;
+    private InputTableCache m_inputTableCache;
 
     private final AtomicReference<BufferedDataTable> m_outputBufferTableReference;
-
-    private final boolean m_inputTableIsAvailable;
 
     private final Runnable m_cleanUpTableViewDataService;
 
     ExpressionRowFilterNodeScriptingService(final AtomicReference<BufferedDataTable> outputTableRef,
         final Runnable cleanUpTableViewDataService) {
         super(null, ExpressionRunnerUtils.SUPPORTED_FLOW_VARIABLE_TYPES_SET::contains);
-
-        var inputData = getWorkflowControl().getInputData();
-        m_inputTableIsAvailable = inputData.length > 0 && inputData[0] != null;
-
-        if (m_inputTableIsAvailable) {
-            m_outputBufferTableReference = outputTableRef;
-            m_outputBufferTableReference.set(getInputTable().getBufferedTable());
-        } else {
-            m_outputBufferTableReference = outputTableRef;
-        }
-
+        m_outputBufferTableReference = outputTableRef;
         m_cleanUpTableViewDataService = cleanUpTableViewDataService;
+        m_inputTableCache = new InputTableCache((BufferedDataTable)getWorkflowControl().getInputData()[0]);
     }
 
     @Override
@@ -137,39 +109,15 @@ final class ExpressionRowFilterNodeScriptingService extends ScriptingService {
     @Override
     public void onDeactivate() {
         m_columnToType = null;
-        m_inputTable = null;
+        m_inputTableCache = null;
     }
 
-    synchronized Function<String, ReturnResult<ValueType>> getColumnToTypeMapper() {
+    private synchronized Function<String, ReturnResult<ValueType>> getColumnToTypeMapper() {
         if (m_columnToType == null) {
             var spec = (DataTableSpec)getWorkflowControl().getInputSpec()[0];
             m_columnToType = ExpressionRunnerUtils.columnToTypesForTypeInference(spec);
         }
         return m_columnToType;
-    }
-
-    synchronized ReferenceTable getInputTable() {
-
-        if (m_inputTable == null) {
-            var inTable = (BufferedDataTable)getWorkflowControl().getInputData()[0];
-            if (inTable == null) {
-                throw new IllegalStateException("Input table not available");
-            }
-            m_inputTableRowCount = inTable.size();
-
-            var nodeContainer = (NativeNodeContainer)NodeContext.getContext().getNodeContainer();
-
-            var executionContext = nodeContainer.createExecutionContext();
-
-            try {
-                // Progress isn't used in this context so we can pass whatever and don't expect it to be canceled
-                m_inputTable = ExpressionRunnerUtils.createReferenceTable(inTable, executionContext,
-                    executionContext.createSubProgress(1), PREVIEW_MAX_ROWS);
-            } catch (CanceledExecutionException ex) {
-                throw new IllegalStateException("Input table preparation for expression cancelled by the user", ex);
-            }
-        }
-        return m_inputTable;
     }
 
     public final class ExpressionNodeRpcService extends RpcService {
@@ -225,75 +173,42 @@ final class ExpressionRowFilterNodeScriptingService extends ScriptingService {
             return diagnostics;
         }
 
-        public void runRowFilterExpression(final String script, int numPreviewRows) {
+        public void runRowFilterExpression(final String script, final int numPreviewRows)
+            throws ExpressionCompileException, VirtualTableIncompatibleException {
 
             if (numPreviewRows > PREVIEW_MAX_ROWS) {
                 throw new IllegalArgumentException("Number of preview rows must be at most 1000");
             }
 
-            var inputTable = getInputTable();
+            var nodeContainer = (NativeNodeContainer)NodeContext.getContext().getNodeContainer();
+            var executionContext = nodeContainer.createExecutionContext();
 
-            final Ast expression;
             try {
-                // Prepare the expression
-                expression = getPreparedExpression(script);
+                var inColTable = m_inputTableCache.getTable(numPreviewRows);
+                var outputTable = ExpressionRowFilterNodeModel.applyFilterExpression( //
+                    script, //
+                    inColTable, //
+                    getSupportedFlowVariablesMap(), //
+                    executionContext, //
+                    this::handleWarningMessage //
+                );
 
-            } catch (ExpressionCompileException ex) {
-                NodeLogger.getLogger(ExpressionRowFilterNodeScriptingService.class)
-                    .debug("Error while running expression in dialog. This should not happen because the "
-                        + "run button is disabled if the expression is invalid: " + ex.getMessage(), ex);
-                addConsoleOutputEvent(new ConsoleText("Error: " + ex.getMessage(), true));
-
-                throw new IllegalStateException(
-                    "Implementation error: Error while running expression in dialog: '%s'".formatted(ex.getMessage()),
-                    ex);
-            }
-            // NB: We use the inRefTable because it is guaranteed to be a columnar table
-            try {
-                ExpressionRunnerUtils.evaluateAggregations(expression, inputTable.getBufferedTable(),
-                    new ExecutionMonitor(), numPreviewRows);
-            } catch (CanceledExecutionException ex) {
-                throw new IllegalStateException("This is an implementation error. Must not happen "
-                    + "because canceling the execution should not be possible.", ex);
-            }
-
-            List<String> warnings = new ArrayList<>();
-            EvaluationContext evaluationContext = warnings::add;
-            numPreviewRows = (int)Math.min(numPreviewRows, m_inputTableRowCount);
-
-            var slicedInputTable = inputTable.getVirtualTable().slice(0, numPreviewRows);
-
-            var exprContext = new NodeExpressionMapperContext(types -> getSupportedFlowVariablesMap());
-            var filteredTable = ExpressionRunnerUtils.filterTableByExpression(slicedInputTable, expression,
-                numPreviewRows, evaluationContext, exprContext);
-
-            var exec = ((NativeNodeContainer)NodeContext.getContext().getNodeContainer()).createExecutionContext();
-            BufferedDataTable outputTable;
-            try {
-                outputTable = ColumnarVirtualTableMaterializer.materializer() //
-                    .sources(inputTable.getSources()) //
-                    .materializeRowKey(true) //
-                    .progress((rowIndex, rowKey) -> {
-                    }) //
-                    .executionContext(exec) //
-                    .tableIdSupplier(Node.invokeGetDataRepository(exec)::generateNewID) //
-                    .materialize(filteredTable) //
-                    .getBufferedTable();
-            } catch (VirtualTableIncompatibleException e) {
-                throw new IllegalStateException("This is an implementation error. Must not happen "
-                    + "because the table is guaranteed to be compatible.", e);
+                updateTablePreview(outputTable, (int)inColTable.size());
             } catch (CanceledExecutionException e) {
-                throw new IllegalStateException("Preview evaluation cancelled by the user, which should be impossible",
-                    e);
+                throw new IllegalStateException("This is an implementation error. Must not happen "
+                    + "because canceling the execution should not be possible.", e);
             }
+        }
+
+        private void updateTablePreview(final BufferedDataTable outputTable, final int numPreviewRows) {
             m_outputBufferTableReference.set(outputTable);
             m_cleanUpTableViewDataService.run();
-            updateOutputTable(numPreviewRows, m_inputTableRowCount);
+            updateOutputTable(numPreviewRows, m_inputTableCache.getFullRowCount());
+        }
 
-            for (var warning : warnings) {
-                addConsoleOutputEvent(new ConsoleText(formatWarning(warning), true));
-            }
-
+        private void handleWarningMessage(final String warningMessage) {
+            // TODO(AP-23152) show warning next to the expression (only the first one)
+            addConsoleOutputEvent(new ConsoleText(formatWarning(warningMessage), true));
         }
 
         private static String formatWarning(final String warningText) {
