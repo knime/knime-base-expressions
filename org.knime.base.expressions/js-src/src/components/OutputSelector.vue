6bcfc786854e5d0e5cb079d801c16eee337118ff
David Hickey <david.hickey@tngtech.com>
2024-10-16 17:55:25 +0200

    AP-23446: OutputSelector can be reconfigured even w/o connected input node
    AP-23446 (Expression cannot be reconfigured if replaced column/variable is missing and there is no alternative)

--- org.knime.base.expressions/js-src/src/components/OutputSelector.vue
+++ org.knime.base.expressions/js-src/src/components/OutputSelector.vue
@@ -58,6 +58,9 @@ const allowedOperationModes = [
   { id: "REPLACE_EXISTING", text: "Replace" },
 ];
 
+const isStringFalsy = (s: string | null | undefined): boolean =>
+  !s || s.trim() === "";
+
 const paintFocus = useShouldFocusBePainted();
 </script>
 
@@ -67,7 +70,12 @@ const paintFocus = useShouldFocusBePainted();
       <ValueSwitch
         v-model="modelValue.outputMode"
         :possible-values="allowedOperationModes"
-        :disabled="props.allowedReplacementEntities.length === 0 || readOnly"
+        :disabled="
+          (props.allowedReplacementEntities.length === 0 &&
+            modelValue.outputMode === 'APPEND' &&
+            isStringFalsy(modelValue.replace)) ||
+          readOnly
+        "
         class="switch-button"
         :class="{ 'focus-painted': paintFocus }"
         compact
