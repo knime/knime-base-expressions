8ea38af0dcc4c1e93217b6dd5019b5ba4a84a49c
Tobias Kampmann <tobias.kampmann@tngtech.com>
2024-10-18 17:32:46 +0200

    AP-23375: Change to 'block: nearest' for scrollIntoView
    AP-23375 (When scrolling the editor into view the collapsed console shifts into view)

--- org.knime.base.expressions/js-src/src/components/MultiEditorContainer.vue
+++ org.knime.base.expressions/js-src/src/components/MultiEditorContainer.vue
@@ -167,7 +167,7 @@ const onFocusChanged = (key: string) => {
       .editor.value?.getDomNode()
       ?.scrollIntoView({
         behavior: "smooth",
-        block: "center",
+        block: "nearest",
       });
   });
   emitOnChange();
