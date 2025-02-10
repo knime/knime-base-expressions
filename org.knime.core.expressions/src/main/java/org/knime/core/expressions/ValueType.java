knime/knime-core-table

wip - functions

org.knime.core.expressions/src/main/java/org/knime/core/expressions/ValueType.java
@@ -72,6 +72,24 @@ public interface ValueType {
     /** Type for "STRING" values */
     ValueType STRING = NativeValueType.STRING;
 
+    /** Type for "DATE" values */
+    ValueType LOCAL_DATE = NativeValueType.DATE;
+
+    /** Type for "TIME" values */
+    ValueType LOCAL_TIME = NativeValueType.TIME;
+
+    /** Type for DATE_TIME values */
+    ValueType LOCAL_DATE_TIME = NativeValueType.DATE_TIME;
+
+    /** Type for ZONED_DATE_TIME values */
+    ValueType ZONED_DATE_TIME = NativeValueType.ZONED_DATE_TIME;
+
+    /** Type for "PERIOD" values */
+    ValueType PERIOD = NativeValueType.PERIOD;
+
+    /** Type for "DURATION" values */
+    ValueType DURATION = NativeValueType.DURATION;
+
     /** Type for values of {@link #BOOLEAN} that might be "MISSING" */
     ValueType OPT_BOOLEAN = new OptionalValueType(NativeValueType.BOOLEAN);
 
@@ -83,6 +101,24 @@ public interface ValueType {
 
     /** Type for values of {@link #STRING} that might be "MISSING" */
     ValueType OPT_STRING = new OptionalValueType(NativeValueType.STRING);
+
+    /** Type for values of {@link #LOCAL_DATE} that might be "MISSING" */
+    ValueType OPT_DATE = new OptionalValueType(NativeValueType.DATE);
+
+    /** Type for values of {@link #LOCAL_TIME} that might be "MISSING" */
+    ValueType OPT_TIME = new OptionalValueType(NativeValueType.TIME);
+
+    /** Type for values of {@link #LOCAL_DATE_TIME} that might be "MISSING" */
+    ValueType OPT_DATE_TIME = new OptionalValueType(NativeValueType.DATE_TIME);
+
+    /** Type for values of {@link #ZONED_DATE_TIME} that might be "MISSING" */
+    ValueType OPT_ZONED_DATE_TIME = new OptionalValueType(NativeValueType.ZONED_DATE_TIME);
+
+    /** Type for values of {@link #PERIOD} that might be "MISSING" */
+    ValueType OPT_PERIOD = new OptionalValueType(NativeValueType.PERIOD);
+
+    /** Type for values of {@link #DURATION} that might be "MISSING" */
+    ValueType OPT_DURATION = new OptionalValueType(NativeValueType.DURATION);
 
     /**
      * Helper to create {@link #BOOLEAN} or {@link #OPT_BOOLEAN} types.
@@ -125,6 +161,46 @@ public interface ValueType {
     }
 
     /**
+     * Helper to create {@link #LOCAL_DATE} or {@link #OPT_DATE} types.
+     *
+     * @param optional if the resulting type should permit "MISSING" values
+     * @return {@link #LOCAL_DATE} or {@link #OPT_DATE}
+     */
+    static ValueType LOCAL_DATE(final boolean optional) { // NOSONAR - naming this utility the same is useful
+        return optional ? OPT_DATE : LOCAL_DATE;
+    }
+
+    /**
+     * Helper to create {@link #LOCAL_TIME} or {@link #OPT_TIME} types.
+     *
+     * @param optional if the resulting type should permit "MISSING" values
+     * @return {@link #LOCAL_TIME} or {@link #OPT_TIME}
+     */
+    static ValueType LOCAL_TIME(final boolean optional) { // NOSONAR - naming this utility the same is useful
+        return optional ? OPT_TIME : LOCAL_TIME;
+    }
+
+    /**
+     * Helper to create {@link #LOCAL_DATE_TIME} or {@link #OPT_DATE_TIME} types.
+     *
+     * @param optional if the resulting type should permit "MISSING" values
+     * @return {@link #LOCAL_DATE_TIME} or {@link #OPT_DATE_TIME}
+     */
+    static ValueType LOCAL_DATE_TIME(final boolean optional) { // NOSONAR - naming this utility the same is useful
+        return optional ? OPT_DATE_TIME : LOCAL_DATE_TIME;
+    }
+
+    /**
+     * Helper to create {@link #ZONED_DATE_TIME} or {@link #OPT_ZONED_DATE_TIME} types.
+     *
+     * @param optional if the resulting type should permit "MISSING" values
+     * @return {@link #ZONED_DATE_TIME} or {@link #OPT_ZONED_DATE_TIME}
+     */
+    static ValueType ZONED_DATE_TIME(final boolean optional) { // NOSONAR - naming this utility the same is useful
+        return optional ? OPT_ZONED_DATE_TIME : ZONED_DATE_TIME;
+    }
+
+    /**
      * Helper to check equality of two types, including optional types.
      *
      * @param type1 type 1
@@ -156,6 +232,29 @@ public interface ValueType {
     }
 
     /**
+     * Helper to check if the given type is temporal and not optional.
+     *
+     * @param type the type to check
+     * @return <code>true</code> if the type is a temporal type (either {@link #LOCAL_DATE}, {@link #LOCAL_TIME},
+     *         {@link #LOCAL_DATE_TIME} or {@link #ZONED_DATE_TIME})
+     */
+    static boolean isTemporal(final ValueType type) {
+        return type.equals(LOCAL_DATE) || type.equals(LOCAL_TIME) || type.equals(LOCAL_DATE_TIME)
+            || type.equals(ZONED_DATE_TIME);
+    }
+
+    /**
+     * Helper to check if the given type is temporal (optional or not).
+     *
+     * @param type the type to check
+     * @return <code>true</code> if the base-type is a temporal type (either {@link #LOCAL_DATE}, {@link #LOCAL_TIME},
+     *         {@link #LOCAL_DATE_TIME} or {@link #ZONED_DATE_TIME})
+     */
+    static boolean isTemporalOrOpt(final ValueType type) {
+        return isTemporal(type.baseType());
+    }
+
+    /**
      * @return a unique but simple name of the type
      */
     String name();
@@ -177,10 +276,13 @@ public interface ValueType {
      */
     ValueType optionalType();
 
-    /** Implementation of {@link ValueType} for the native types "BOOLEAN", "INTEGER, "FLOAT", and "STRING". */
+    /**
+     * Implementation of {@link ValueType} for the native types "BOOLEAN", "INTEGER, "FLOAT", "STRING", "DATE", "TIME",
+     * "DATE_TIME", "ZONED_DATE_TIME", "PERIOD", "DURATION". .
+     */
     @SuppressWarnings("hiding")
     enum NativeValueType implements ValueType {
-            BOOLEAN, INTEGER, FLOAT, STRING;
+            BOOLEAN, INTEGER, FLOAT, STRING, DATE, TIME, DATE_TIME, ZONED_DATE_TIME, PERIOD, DURATION;
 
         @Override
         public boolean isOptional() {
@@ -245,7 +347,7 @@ public interface ValueType {
 
         @Override
         public String name() {
-            return m_baseType.name() + "| MISSING" ;
+            return m_baseType.name() + "| MISSING";
         }
 
         @Override

org.knime.core.expressions/src/main/java/org/knime/core/expressions/functions/BuiltInFunctions.java
@@ -98,11 +98,15 @@ public final class BuiltInFunctions {
         ControlFlowFunctions.CATEGORY //
     );
 
+    /** List of all date and time categories */
+    public static final List<OperatorCategory> META_CATEGORY_DATE_TIME = List.of( //
+        DateTimeFunctions.CATEGORY_GENERAL //
+    );
+
     /** All built-in function categories */
     public static final List<OperatorCategory> BUILT_IN_CATEGORIES = Stream.concat( //
-        Stream.concat( //
-            META_CATEGORY_MATH.stream(), META_CATEGORY_STRING.stream()), //
-        META_CATEGORY_CONTROL.stream() //
+        Stream.concat(META_CATEGORY_MATH.stream(), META_CATEGORY_STRING.stream()), //
+        Stream.concat(META_CATEGORY_CONTROL.stream(), META_CATEGORY_DATE_TIME.stream()) //
     ).collect(Collectors.toUnmodifiableList());
 
     /** Built-in functions */
@@ -206,7 +210,9 @@ public final class BuiltInFunctions {
         // String – Encode
         StringFunctions.XML_ENCODE, //
         StringFunctions.URL_ENCODE, //
-        StringFunctions.URL_DECODE //
+        StringFunctions.URL_DECODE, //
+        // Date&Time – General
+        DateTimeFunctions.TIME //
     );
 
     /** Built-in functions as map */

knime/knime-base-expressions

org.knime.core.expressions/src/main/java/org/knime/core/expressions/ValueType.java
@@ -72,6 +72,24 @@ public interface ValueType {
     /** Type for "STRING" values */
     ValueType STRING = NativeValueType.STRING;
 
+    /** Type for "DATE" values */
+    ValueType LOCAL_DATE = NativeValueType.DATE;
+
+    /** Type for "TIME" values */
+    ValueType LOCAL_TIME = NativeValueType.TIME;
+
+    /** Type for DATE_TIME values */
+    ValueType LOCAL_DATE_TIME = NativeValueType.DATE_TIME;
+
+    /** Type for ZONED_DATE_TIME values */
+    ValueType ZONED_DATE_TIME = NativeValueType.ZONED_DATE_TIME;
+
+    /** Type for "PERIOD" values */
+    ValueType PERIOD = NativeValueType.PERIOD;
+
+    /** Type for "DURATION" values */
+    ValueType DURATION = NativeValueType.DURATION;
+
     /** Type for values of {@link #BOOLEAN} that might be "MISSING" */
     ValueType OPT_BOOLEAN = new OptionalValueType(NativeValueType.BOOLEAN);
 
@@ -83,6 +101,24 @@ public interface ValueType {
 
     /** Type for values of {@link #STRING} that might be "MISSING" */
     ValueType OPT_STRING = new OptionalValueType(NativeValueType.STRING);
+
+    /** Type for values of {@link #LOCAL_DATE} that might be "MISSING" */
+    ValueType OPT_LOCAL_DATE = new OptionalValueType(NativeValueType.DATE);
+
+    /** Type for values of {@link #LOCAL_TIME} that might be "MISSING" */
+    ValueType OPT_LOCAL_TIME = new OptionalValueType(NativeValueType.TIME);
+
+    /** Type for values of {@link #LOCAL_DATE_TIME} that might be "MISSING" */
+    ValueType OPT_LOCAL_DATE_TIME = new OptionalValueType(NativeValueType.DATE_TIME);
+
+    /** Type for values of {@link #ZONED_DATE_TIME} that might be "MISSING" */
+    ValueType OPT_ZONED_DATE_TIME = new OptionalValueType(NativeValueType.ZONED_DATE_TIME);
+
+    /** Type for values of {@link #PERIOD} that might be "MISSING" */
+    ValueType OPT_PERIOD = new OptionalValueType(NativeValueType.PERIOD);
+
+    /** Type for values of {@link #DURATION} that might be "MISSING" */
+    ValueType OPT_DURATION = new OptionalValueType(NativeValueType.DURATION);
 
     /**
      * Helper to create {@link #BOOLEAN} or {@link #OPT_BOOLEAN} types.
@@ -125,6 +161,46 @@ public interface ValueType {
     }
 
     /**
+     * Helper to create {@link #LOCAL_DATE} or {@link #OPT_LOCAL_DATE} types.
+     *
+     * @param optional if the resulting type should permit "MISSING" values
+     * @return {@link #LOCAL_DATE} or {@link #OPT_LOCAL_DATE}
+     */
+    static ValueType LOCAL_DATE(final boolean optional) { // NOSONAR - naming this utility the same is useful
+        return optional ? OPT_LOCAL_DATE : LOCAL_DATE;
+    }
+
+    /**
+     * Helper to create {@link #LOCAL_TIME} or {@link #OPT_LOCAL_TIME} types.
+     *
+     * @param optional if the resulting type should permit "MISSING" values
+     * @return {@link #LOCAL_TIME} or {@link #OPT_LOCAL_TIME}
+     */
+    static ValueType LOCAL_TIME(final boolean optional) { // NOSONAR - naming this utility the same is useful
+        return optional ? OPT_LOCAL_TIME : LOCAL_TIME;
+    }
+
+    /**
+     * Helper to create {@link #LOCAL_DATE_TIME} or {@link #OPT_LOCAL_DATE_TIME} types.
+     *
+     * @param optional if the resulting type should permit "MISSING" values
+     * @return {@link #LOCAL_DATE_TIME} or {@link #OPT_LOCAL_DATE_TIME}
+     */
+    static ValueType LOCAL_DATE_TIME(final boolean optional) { // NOSONAR - naming this utility the same is useful
+        return optional ? OPT_LOCAL_DATE_TIME : LOCAL_DATE_TIME;
+    }
+
+    /**
+     * Helper to create {@link #ZONED_DATE_TIME} or {@link #OPT_ZONED_DATE_TIME} types.
+     *
+     * @param optional if the resulting type should permit "MISSING" values
+     * @return {@link #ZONED_DATE_TIME} or {@link #OPT_ZONED_DATE_TIME}
+     */
+    static ValueType ZONED_DATE_TIME(final boolean optional) { // NOSONAR - naming this utility the same is useful
+        return optional ? OPT_ZONED_DATE_TIME : ZONED_DATE_TIME;
+    }
+
+    /**
      * Helper to check equality of two types, including optional types.
      *
      * @param type1 type 1
@@ -156,6 +232,29 @@ public interface ValueType {
     }
 
     /**
+     * Helper to check if the given type is temporal and not optional.
+     *
+     * @param type the type to check
+     * @return <code>true</code> if the type is a temporal type (either {@link #LOCAL_DATE}, {@link #LOCAL_TIME},
+     *         {@link #LOCAL_DATE_TIME} or {@link #ZONED_DATE_TIME})
+     */
+    static boolean isTemporal(final ValueType type) {
+        return type.equals(LOCAL_DATE) || type.equals(LOCAL_TIME) || type.equals(LOCAL_DATE_TIME)
+            || type.equals(ZONED_DATE_TIME);
+    }
+
+    /**
+     * Helper to check if the given type is temporal (optional or not).
+     *
+     * @param type the type to check
+     * @return <code>true</code> if the base-type is a temporal type (either {@link #LOCAL_DATE}, {@link #LOCAL_TIME},
+     *         {@link #LOCAL_DATE_TIME} or {@link #ZONED_DATE_TIME})
+     */
+    static boolean isTemporalOrOpt(final ValueType type) {
+        return isTemporal(type.baseType());
+    }
+
+    /**
      * @return a unique but simple name of the type
      */
     String name();
@@ -177,10 +276,13 @@ public interface ValueType {
      */
     ValueType optionalType();
 
-    /** Implementation of {@link ValueType} for the native types "BOOLEAN", "INTEGER, "FLOAT", and "STRING". */
+    /**
+     * Implementation of {@link ValueType} for the native types "BOOLEAN", "INTEGER, "FLOAT", "STRING", "DATE", "TIME",
+     * "DATE_TIME", "ZONED_DATE_TIME", "PERIOD", "DURATION". .
+     */
     @SuppressWarnings("hiding")
     enum NativeValueType implements ValueType {
-            BOOLEAN, INTEGER, FLOAT, STRING;
+            BOOLEAN, INTEGER, FLOAT, STRING, DATE, TIME, DATE_TIME, ZONED_DATE_TIME, PERIOD, DURATION;
 
         @Override
         public boolean isOptional() {
@@ -245,7 +347,7 @@ public interface ValueType {
 
         @Override
         public String name() {
-            return m_baseType.name() + "| MISSING" ;
+            return m_baseType.name() + "| MISSING";
         }
 
         @Override

org.knime.core.expressions/src/main/java/org/knime/core/expressions/functions/BuiltInFunctions.java
@@ -98,11 +98,15 @@ public final class BuiltInFunctions {
         ControlFlowFunctions.CATEGORY //
     );
 
+    /** List of all date and time categories */
+    public static final List<OperatorCategory> META_CATEGORY_DATE_TIME = List.of( //
+        DateTimeFunctions.CATEGORY_GENERAL //
+    );
+
     /** All built-in function categories */
     public static final List<OperatorCategory> BUILT_IN_CATEGORIES = Stream.concat( //
-        Stream.concat( //
-            META_CATEGORY_MATH.stream(), META_CATEGORY_STRING.stream()), //
-        META_CATEGORY_CONTROL.stream() //
+        Stream.concat(META_CATEGORY_MATH.stream(), META_CATEGORY_STRING.stream()), //
+        Stream.concat(META_CATEGORY_CONTROL.stream(), META_CATEGORY_DATE_TIME.stream()) //
     ).collect(Collectors.toUnmodifiableList());
 
     /** Built-in functions */
@@ -206,7 +210,9 @@ public final class BuiltInFunctions {
         // String – Encode
         StringFunctions.XML_ENCODE, //
         StringFunctions.URL_ENCODE, //
-        StringFunctions.URL_DECODE //
+        StringFunctions.URL_DECODE, //
+        // Date&Time – General
+        DateTimeFunctions.PARSE_TIME //
     );
 
     /** Built-in functions as map */