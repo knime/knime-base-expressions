ff17cec0a58c1ff62bbc6499ea9ff95bc88f25ec
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2025-08-25 17:23:26 +0200

    AP-24709: Call init function in main scripts
    And call the mocked init with the browser-mocks if we are using the
    browser mock.
    
    AP-24709 (Do not initialize on import in @knime/scripting-editor but export async initialize function)

--- org.knime.base.expressions/js-src/src/rowFilterApp/expressionRowFilter.ts
+++ org.knime.base.expressions/js-src/src/rowFilterApp/expressionRowFilter.ts
@@ -1,9 +1,20 @@
-import "./__mocks__/browser-mock-row-filter-services";
 import { createApp } from "vue";
+
+import { init, initMocked } from "@knime/scripting-editor";
 
 import { setupConsola } from "@/common/functions";
 import ExpressionRowFilterApp from "@/rowFilterApp/ExpressionRowFilterApp.vue";
 
 setupConsola();
+
+// Initialize application (e.g., load initial data, set up services)
+if (import.meta.env.MODE === "development.browser") {
+  // Mock the initial data and services
+  initMocked(
+    (await import("./__mocks__/browser-mock-row-filter-services")).default,
+  );
+} else {
+  await init();
+}
 
 createApp(ExpressionRowFilterApp).mount("#app");
